5e564ef8da9a1d039d91299e39dd8fff7e99818a
sjamillah <j.ssozi@alustudent.com>
2026-03-23 08:54:01 +0200

    feat: created the exceptions for the data importer

---
+++ amalitech_training/clean_code_testing_and_git/LAB_1/exceptions.py
@@ -0,0 +1,25 @@
+"""Custom exceptions for the CSV importer."""
+ 
+ 
+class ImporterError(Exception):
+    """Base for all importer errors."""
+ 
+ 
+class FileFormatError(ImporterError):
+    """File is missing, unreadable, or not valid UTF-8."""
+ 
+ 
+class MissingFieldError(FileFormatError):
+    """A required column is absent in a row."""
+ 
+ 
+class ValidationError(ImporterError):
+    """A field value fails a business rule."""
+ 
+ 
+class InvalidEmailError(ValidationError):
+    """Email does not match a valid format."""
+ 
+ 
+class DuplicateUserError(ImporterError):
+    """user_id already exists in the database."""
